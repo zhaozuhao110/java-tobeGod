e6843868984144170dbaf727df52b7942d588dec
zzuh <zzuh@servyou.mail.com.cn>
2025-03-05 19:01:23 +0800

    init

---
+++ god-base/src/main/java/god/github/haoer/godbase/GodBaseApplication.java
@@ -0,0 +1,13 @@
+package god.github.haoer.godbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GodBaseApplication.class, args);
+    }
+
+}
---
+++ god-base/src/test/java/god/github/haoer/godbase/GodBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package god.github.haoer.godbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
